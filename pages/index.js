76887148d758f377d4af5c6e1c2b01d6ec4494d4
Jeremy Ott <jeremy.ott@bullhorn.com>
2021-04-29 06:20:44 -0400

    adding new content to primary maintenance page

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import styles from '@components/Footer.module.css'
+import { Button } from 'react-bootstrap';
 
 export default function Home() {
   return (
