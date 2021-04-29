b41d1abbcea5f07f6563538e898defc93807f950
Jeremy Ott <jeremy.ott@bullhorn.com>
2021-04-28 22:24:12 -0400

    updating the footer content

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,8 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with nothing but <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+      &copy; A Alliance 2021 
+      {/* <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> */}
       </footer>
     </>
   )
