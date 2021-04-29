f77006001458263fc12b32561b34f24afc73ffcd
Jeremy Ott <jeremy.ott@bullhorn.com>
2021-04-28 22:54:49 -0400

    updating index.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-      &copy; A Alliance 2021 
+      &copy; 2021 A Alliance
       {/* <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> */}
       </footer>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,23 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import styles from '@components/Footer.module.css'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>A Aliance - Coming Soon!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} />
+        <Header title="A whole new experience is in the works!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          There's an art to forming an office. See what we've helped our clients create!
         </p>
+        <a href="http://www.daniarps.com/"><button>Our Portfolio</button></a>
       </main>
 
       <Footer />
